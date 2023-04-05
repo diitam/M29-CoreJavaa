fe9dffadfceb12f0d2121eecfc033112ae54760d
Disha Tamore <dishatamore2001@gmail.com>
2023-04-05 08:55:27 +0530

    Commit1

---
+++ src/dayone/DataTypes.java
@@ -0,0 +1,26 @@
+package dayone;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		int num1 = 5/2; //it will give whole number
+
+		float num2 = 109/45; //it will give decimal number
+
+		/*if we use d after any number for double type it will give the
+		* precision upto 16 decimal places.
+		*/
+		double num3 = 10d/3d;
+
+		/*if we use f after any number for float type it will give the
+		* precision upto 7 decimal places.
+		*/
+		float num4 = 14f/3f;
+
+		System.out.println("The num1 is: "+num1);
+		System.out.println("The num2 is: "+num2);
+		System.out.println("The num3 is: "+num3);
+		System.out.println("The num4 is: "+num4);
+	}
+
+}

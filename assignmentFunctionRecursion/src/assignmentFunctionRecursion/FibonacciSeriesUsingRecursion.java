283e725259cd1ac973405dd535bec5784a6334a3
Disha Tamore <dishatamore2001@gmail.com>
2023-04-10 22:33:06 +0530

    Commit-2

--- assignmentFunctionRecursion/src/assignmentFunctionRecursion/FibonacciSeriesUsingRecursion.java
+++ assignmentFunctionRecursion/src/assignmentFunctionRecursion/FibonacciSeriesUsingRecursion.java
@@ -3,14 +3,14 @@ import java.util.*;
 public class FibonacciSeriesUsingRecursion {
 
 	 static int n1=0,n2=1,n3=0;    
-	 static void printFibonacci(int count){    
+	 static void Fibonacci(int count){    
 	    if(count>0)
 	    {    
 	         n3 = n1 + n2;    
 	         n1 = n2;    
 	         n2 = n3;    
 	         System.out.print(" "+n3);   
-	         printFibonacci(count-1);    
+	         Fibonacci(count-1);    
 	     }    
 	 }    
 	 public static void main(String args[]){    
@@ -18,7 +18,7 @@ public class FibonacciSeriesUsingRecursion {
 		 System.out.println("Enter the number: ");
 	int count=sc.nextInt();
 	  System.out.print(n1+" "+n2);//printing 0 and 1    
-	  printFibonacci(count-2);//n-2 because 2 numbers are already printed   
+	  Fibonacci(count-2);//n-2 because 2 numbers are already printed   
 	  sc.close();
 	 }  
 		  

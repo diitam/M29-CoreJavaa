b3f6719392083407ffa08495d5b7739922990b95
Disha Tamore <dishatamore2001@gmail.com>
2023-05-03 20:11:21 +0530

    Commit 1

---
+++ daynine/src/daynine/arrays/ArraysRunTime.java
@@ -0,0 +1,27 @@
+package daynine.arrays;
+
+import java.util.Scanner;
+
+public class ArraysRunTime {
+
+	public static void main(String[] args) {
+
+		System.out.println("Enter the no. of elements: ");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		//array declaration
+		int arr[]=new int[num];
+		//array initialization
+		for(int i=0; i<num; i++) 
+		{
+			arr[i]=sc.nextInt();
+		}
+		//printing the array
+		for(int itr:arr) 
+		{
+			System.out.println("Array elements are: "+itr);
+		}
+		sc.close();
+	}
+
+}

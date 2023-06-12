f3d53993b6c0362a67ffebb1adae158d7fc69fab
Disha Tamore <dishatamore2001@gmail.com>
2023-06-12 15:16:52 +0530

    Commit 1

---
+++ dayseventeen/src/dayseventeen/datetime/ClockClassExample.java
@@ -0,0 +1,18 @@
+package dayseventeen.datetime;
+
+import java.time.Clock;
+
+public class ClockClassExample {
+
+
+	public static void main(String[] args) {
+		Clock c1 = Clock.systemUTC();
+		System.out.println(c1);
+		
+		Clock c2 = Clock.systemDefaultZone();
+		System.out.println(c2);
+
+	}
+
+}
+

c5ab560a016cfb4cd67987fb7692ce570613b7f6
Disha Tamore <dishatamore2001@gmail.com>
2023-06-14 20:56:03 +0530

    Commit 1

---
+++ daynineteen/src/daynineteen/service/StudentService.java
@@ -0,0 +1,14 @@
+package daynineteen.service;
+
+import java.util.List;
+
+import daynineteen.entities.Student;
+public interface StudentService {
+	
+	public boolean addStudent(Student student);//create
+	public Student getStudent(int rollno);//retrieve with specific id
+	public List<Student>getAll();//retrieve all the data
+	public Student updateStudent(Student student);//update
+	public boolean deleteStudent(int rollno);//delete
+
+}

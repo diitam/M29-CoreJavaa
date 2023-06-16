695f4e625af40c2463b45f314d7fa45f7c361bf3
Disha Rajendra Tamore <78993245+diitam@users.noreply.github.com>
2023-06-16 10:07:17 +0530

    Delete MoneyMoneyBank directory

--- MoneyMoneyBank/src/MoneyMoneyBank/framework/BankFactory.java
+++
@@ -1,8 +0,0 @@
-package MoneyMoneyBank.framework;
-
-public abstract class BankFactory {
-	
-	abstract public SavingAcc getNewSavingAcc(int accNo, String accNm, Float accBal, boolean isSalaried);
-	abstract public CurrentAcc getNewCurrentAcc(int accNo, String accNm, Float accBal, float creditLimit);
-}
-
